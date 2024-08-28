fdccc794a17541df24d786c03b73918bcf58269d
Rodrigo González Wilkens <libra.rgw@gmail.com>
2024-08-28 09:34:05 -0300

    +inertia;vue;shadcn;tailwindcss;

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { Button } from "@/Components/ui/button";
+import { Switch } from "@/components/ui/switch";
+</script>
+
+<template>
+    <div class="container max-w-screen-lg mx-auto">
+        <h1 class="font-serif font-normal">Home</h1>
+        <h1 class="font-serif font-medium">Home</h1>
+        <h1 class="font-serif font-semibold">Home</h1>
+        <h1 class="font-serif font-bold">Home</h1>
+        <button class="text-black bg-rose-400">Button</button>
+        <p class="font-normal text-rose-500">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Consequatur neque soluta eos!
+        </p>
+        <p class="font-medium">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Consequatur neque soluta eos!
+        </p>
+        <p class="font-semibold">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Consequatur neque soluta eos!
+        </p>
+        <p class="font-bold">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Consequatur neque soluta eos!
+        </p>
+        <Button variant="default" size="lg">Button</Button>
+        <Switch />
+    </div>
+</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,45 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
+const animate = require("tailwindcss-animate");
 
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
+    darkMode: ["class"],
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.vue',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./resources/js/**/*.{ts,tsx,vue}",
     ],
-
+    prefix: "",
     theme: {
+        container: {
+            center: true,
+            padding: "2rem",
+            screens: {
+                "2xl": "1400px",
+            },
+        },
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Gothic A1", ...defaultTheme.fontFamily.sans],
+                serif: ["Playfair Display", ...defaultTheme.fontFamily.serif],
+            },
+            keyframes: {
+                "accordion-down": {
+                    from: { height: 0 },
+                    to: { height: "var(--radix-accordion-content-height)" },
+                },
+                "accordion-up": {
+                    from: { height: "var(--radix-accordion-content-height)" },
+                    to: { height: 0 },
+                },
+            },
+            animation: {
+                "accordion-down": "accordion-down 0.2s ease-out",
+                "accordion-up": "accordion-up 0.2s ease-out",
             },
         },
     },
-
-    plugins: [forms],
+    plugins: [animate, forms],
 };
